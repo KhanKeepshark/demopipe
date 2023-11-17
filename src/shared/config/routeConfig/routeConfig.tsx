f84f8d7de52810d63db5e05519f6fb4445fbdf4e
Alikhan <auten384@gmail.com>
2023-11-17 11:18:44 +0600

    fix: fix routes

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,33 +1,38 @@
 import { RouteProps } from "react-router-dom";
 import { MainPage } from "@/pages/MainPage";
-import { ReverseLegBending } from "@/widgets/reverseLegBending";
-import { LyingLegLifting } from "@/widgets/lyingLegLifting";
 
 export enum AppRoutes {
-  MAIN = "main",
-  LyingLegLiftingLeft = "testOne",
-  ReverseLegBending = "testTwo",
+  // MAIN = "main",
+  // LyingLegLiftingLeft = "testOne",
+  // ReverseLegBending = "testTwo",
+  TEST = "test",
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
-  [AppRoutes.MAIN]: "/",
-  [AppRoutes.LyingLegLiftingLeft]: "/testOne",
-  [AppRoutes.ReverseLegBending]: "/testTwo",
+  // [AppRoutes.MAIN]: "/",
+  // [AppRoutes.LyingLegLiftingLeft]: "/testOne",
+  // [AppRoutes.ReverseLegBending]: "/testTwo",
+  [AppRoutes.TEST]: "/test",
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
-  [AppRoutes.MAIN]: {
-    path: RoutePath.main,
+  // [AppRoutes.MAIN]: {
+  //   path: RoutePath.main,
+  //   element: <MainPage />,
+  // },
+
+  // [AppRoutes.LyingLegLiftingLeft]: {
+  //   path: RoutePath.testOne,
+  //   element: <LyingLegLifting />,
+  // },
+
+  // [AppRoutes.ReverseLegBending]: {
+  //   path: RoutePath.testTwo,
+  //   element: <ReverseLegBending />,
+  // },
+
+  [AppRoutes.TEST]: {
+    path: RoutePath.test,
     element: <MainPage />,
-  },
-
-  [AppRoutes.LyingLegLiftingLeft]: {
-    path: RoutePath.testOne,
-    element: <LyingLegLifting />,
-  },
-
-  [AppRoutes.ReverseLegBending]: {
-    path: RoutePath.testTwo,
-    element: <ReverseLegBending />,
   },
 };
