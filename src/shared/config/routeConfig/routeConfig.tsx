cbf2248e835e3dea0a2dc92375b5fc6616f148c4
Alikhan <auten384@gmail.com>
2023-11-17 11:47:24 +0600

    test: route test

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -2,24 +2,24 @@ import { RouteProps } from "react-router-dom";
 import { MainPage } from "@/pages/MainPage";
 
 export enum AppRoutes {
-  // MAIN = "main",
+  MAIN = "main",
   // LyingLegLiftingLeft = "testOne",
   // ReverseLegBending = "testTwo",
   TEST = "test",
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
-  // [AppRoutes.MAIN]: "/",
+  [AppRoutes.MAIN]: "/",
   // [AppRoutes.LyingLegLiftingLeft]: "/testOne",
   // [AppRoutes.ReverseLegBending]: "/testTwo",
   [AppRoutes.TEST]: "/test",
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
-  // [AppRoutes.MAIN]: {
-  //   path: RoutePath.main,
-  //   element: <MainPage />,
-  // },
+  [AppRoutes.MAIN]: {
+    path: RoutePath.main,
+    element: <MainPage />,
+  },
 
   // [AppRoutes.LyingLegLiftingLeft]: {
   //   path: RoutePath.testOne,
