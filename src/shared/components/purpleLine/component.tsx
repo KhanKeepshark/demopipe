de48a7e1b05e70768af35faea5d477a8eb4627f8
Alikhan <auten384@gmail.com>
2023-11-23 15:47:58 +0600

    fix: test

--- src/shared/components/purpleLine/component.tsx
+++ src/shared/components/purpleLine/component.tsx
@@ -2,7 +2,7 @@ export const PurpleLine = () => {
   return (
     <>
       <div className="absolute h-1 w-full bg-indigo-500 bottom-20 hidden sm:block" />
-      <div className="absolute w-1 h-full bg-indigo-500 left-16 top-0 sm:hidden" />
+      <div className="absolute h-1 w-full bg-indigo-500 bottom-14 sm:hidden" />
     </>
   );
 };
