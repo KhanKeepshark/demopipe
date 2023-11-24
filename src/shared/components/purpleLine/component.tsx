8e071d82c1d45e6c6ee282ec07cf84505149ae9a
Alikhan <auten384@gmail.com>
2023-11-24 19:47:52 +0600

    fix: change purple line width

--- src/shared/components/purpleLine/component.tsx
+++ src/shared/components/purpleLine/component.tsx
@@ -1,8 +1,8 @@
 export const PurpleLine = () => {
   return (
     <>
-      <div className="absolute h-1 w-full bg-indigo-500 bottom-20 hidden sm:block" />
-      <div className="absolute w-1 h-full bg-indigo-500 left-16 top-0 sm:hidden" />
+      <div className="absolute h-2 w-full bg-indigo-500 border border-black bottom-20 hidden xl:block" />
+      <div className="absolute w-2 h-full bg-indigo-500 border border-black left-14 top-0 xl:hidden" />
     </>
   );
 };
