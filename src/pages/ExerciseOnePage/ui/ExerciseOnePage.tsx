74d9d60a76cf0f5db38f3c12cd7367c5505d9ddd
Alikhan <auten384@gmail.com>
2023-12-08 19:26:46 +0600

    fix

--- src/pages/ExerciseOnePage/ui/ExerciseOnePage.tsx
+++ src/pages/ExerciseOnePage/ui/ExerciseOnePage.tsx
@@ -2,7 +2,7 @@ import { ThirdEx } from "@/widgets/exercises";
 import { useState } from "react";
 
 export const ExerciseOnePage = () => {
-  const [finishFirstExersize, setFinishFirstExersize] = useState(false);
-  const [results, setResults] = useState<number[]>([]);
+  const [, setFinishFirstExersize] = useState(false);
+  const [, setResults] = useState<number[]>([]);
   return <ThirdEx setFinish={setFinishFirstExersize} setResults={setResults} />;
 };
